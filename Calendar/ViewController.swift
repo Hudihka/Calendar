8aa98e90dd508ccb60598b7a71855bbf6c343778
Konstantin Iroshnikov <k.iroshnikov@itmegastar.com>
2020-01-31 14:11:40 +0700

    init commit

---
+++ Calendar/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Calendar
+//
+//  Created by Username on 31.01.2020.
+//  Copyright © 2020 itMegastar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
