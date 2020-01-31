39e034ca9cc45e1bbe9317d87031bf0a79708be2
Konstantin Iroshnikov <k.iroshnikov@itmegastar.com>
2020-01-31 15:25:35 +0700

    создание вью контроллера лет

---
+++ Calendar/Controllers/YearVC/YearViewController.swift
@@ -0,0 +1,22 @@
+//
+//  YearViewController.swift
+//  Calendar
+//
+//  Created by Username on 31.01.2020.
+//  Copyright © 2020 itMegastar. All rights reserved.
+//
+
+import UIKit
+
+class YearViewController: UIViewController {
+
+    @IBOutlet weak var collectionView: UICollectionView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
