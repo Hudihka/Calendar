4b0fac5df4c5510c229b33eed83bb60c9bd0f9cf
Konstantin Iroshnikov <k.iroshnikov@itmegastar.com>
2020-02-05 12:19:55 +0700

    закончен дизайн ячейки

--- Calendar/Controllers/YearVC/Cell/YearsDayCell.swift
+++ Calendar/Controllers/YearVC/Cell/YearsDayCell.swift
@@ -32,12 +32,23 @@ class YearsDayCell: UICollectionViewCell {
 
     private func desingView(){
 
+        let isYearsVC = UIApplication.shared.workVC is YearViewController
+
+        //габариты лейбла
+
+        let sizeLabel = isYearsVC ? widthDayInMonth : 0.8 * widthDay
+        constreintWidthCell.constant = sizeLabel
+
+        labelDay.addRadius(number: sizeLabel * 0.5)
+
+        labelDay.layer.borderWidth = isYearsVC ? 0.5 : 2
+
         guard let day = day else {
             labelDay.text = ""
+            labelDay.layer.borderColor = UIColor.clear.cgColor
             return
         }
-        
-        let isYearsVC = UIApplication.shared.workVC is YearViewController
+
 
         labelDay.text = "\(day.numberMonth)"
         labelDay.alpha = day.isDiapazone ? 1 : 0.3
@@ -47,20 +58,12 @@ class YearsDayCell: UICollectionViewCell {
         //ширифт
 
         let sizeFont: CGFloat = isYearsVC ? 9 : 18
-        labelDay.layer.borderColor = UIColor.clear.cgColor
         labelDay.font = UIFont.systemFont(ofSize: sizeFont, weight: .medium)
-        
-        //габариты лейбла
-        
-        let sizeLabel = isYearsVC ? widthDayInMonth : 0.8 * widthDay
-        constreintWidthCell.constant = sizeLabel
-        
+
         //округлениие вью
-        
-        labelDay.cirkleView()
-        labelDay.layer.borderWidth = 2
-        
-        labelDay.layer.borderColor = day.isTooday ? UIColor.clear.cgColor : UIColor.black.cgColor
+
+        labelDay.layer.borderColor = day.isTooday ? UIColor.black.cgColor : UIColor.clear.cgColor
+
         
     }
 
